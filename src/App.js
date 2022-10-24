8545ea287a2ad83512128b20cce30fc0ac8fae11
Daniel Barrios J <daniel.bj.rd@gmail.com>
2022-10-24 02:00:36 -0500

    ajustes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          AMARANTO
+          Amaranto... en desarrollo
         </p>
       </header>
     </div>

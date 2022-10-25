a0b5a2e16c6cc925c5aad64eb5aff8dba10ef7a7
Daniel Barrios J <daniel.bj.rd@gmail.com>
2022-10-25 04:00:47 -0500

    arreglos eslint

--- src/components/container/Header.jsx
+++ src/components/container/Header.jsx
@@ -10,12 +10,12 @@ const Header = () => {
     <div class="navbar-left navbar">
       <img src="https://scontent.fmex7-1.fna.fbcdn.net/v/t39.30808-6/280951877_112559854786002_6645048926993550950_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeEXMDOvOHKjv7L5sIaLZfUl5KsPiLZKYb_kqw-ItkphvwKFrGU4OoOoHzlz9rgdwAZywN7JQ2Eg0PaRPP1111K-&_nc_ohc=VGePVZ4GfhsAX854UwL&_nc_ht=scontent.fmex7-1.fna&oh=00_AT_efgiVyGOXkPcIMJ_4B4FY1mDUhnmm9LcsJe55yKXLUg&oe=635C6664" alt="logo" class="logo" />
       <ul>
-        <li><a href="#">All</a></li>
-        <li><a href="#">Clothes</a></li>
-        <li><a href="#">Electronics</a></li>
-        <li><a href="#">Fornitures</a></li>
-        <li><a href="#">Toys</a></li>
-        <li><a href="#">Others</a></li>
+        <li><a href="/">All</a></li>
+        <li><a href="/">Clothes</a></li>
+        <li><a href="/">Electronics</a></li>
+        <li><a href="/">Fornitures</a></li>
+        <li><a href="/">Toys</a></li>
+        <li><a href="/">Others</a></li>
       </ul>
     </div>
 

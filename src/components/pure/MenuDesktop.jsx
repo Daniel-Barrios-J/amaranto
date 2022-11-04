e0cd714cbd8ae3002d54af2c76be5e0b01aa8f9f
Daniel Barrios J <daniel.bj.rd@gmail.com>
2022-11-03 23:04:38 -0600

    avances

--- src/components/pure/MenuDesktop.jsx
+++ src/components/pure/MenuDesktop.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/css/menuDesktop.css'
 import { useDispatch } from 'react-redux';
 import { setLog } from '../../store/loginState/reducer';
